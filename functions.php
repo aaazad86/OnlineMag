3d9b7f63fe0a94e1c8ec6c9efc0e23391839daa1
Al Amin Azad <6736991+aaazad86@users.noreply.github.com>
2022-08-23 20:00:26 +0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,1 +1,23 @@
 
+// Regester sidebar 
+function themename_widgets_init() {
+    register_sidebar( array(
+        'name'          => __( 'Right Sidebar', 'theme_name' ),
+        'id'            => 'sidebar-1',
+        'description'   => __( 'Right sidebar of single page or main page.' ),
+        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</aside>',
+        'before_title'  => '<h3 class="widget-title">',
+        'after_title'   => '</h3>',
+    ) );
+ 
+    register_sidebar( array(
+        'name'          => __( 'Left Sidebar', 'theme_name' ),
+        'id'            => 'sidebar-2',
+        'description'   => __( 'Left sidebar of single page or main page.' ),
+        'before_widget' => '<ul><li id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</li></ul>',
+        'before_title'  => '<h3 class="widget-title">',
+        'after_title'   => '</h3>',
+    ) );
+}
